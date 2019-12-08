9a196cfa96d6e29b2345639e2be147c5eab963b6
Andrew Ulrich <andyinthemachine@hotmail.com>
2019-12-08 12:33:18 -0700

    1080p

--- mp4-hls.js
+++ mp4-hls.js
@@ -1,17 +1,29 @@
 
-require("dotenv").config();
-var fs = require("fs");
+require('dotenv').config();
+var fs = require('fs');
+var path = require('path');
 var ffmpeg = require('fluent-ffmpeg');
 var ffprobe = require('fluent-ffmpeg');
 
 const input_file = process.argv[2];
+const base_video_name = path.basename(input_file, '.mp4');
+console.log("video_name = ", base_video_name);
+
+
+try {
+  if (!fs.existsSync(base_video_name)){
+    fs.mkdirSync(base_video_name)
+  }
+} catch (err) {
+  console.error(err)
+}
+
 
 new_line = () => console.log("\n");
 
 
 ffmpeg.getAvailableFormats(function (err, formats) {
   if (err) return console.log(err);
-
   // console.log('Available formats:');
   // console.dir(formats);
 });
@@ -35,15 +47,49 @@ ffmpeg.ffprobe(input_file, function (err, metadata) {
 
 
 // ffmpeg -y -i Alex_SL.mp4 -hls_time 6 -hls_segment_filename "fileSequence%d.ts" -hls_playlist_type vod prog_index.m3u8
+// ffmpeg -y -i sample.mov -hls_time 9 -hls_segment_filename "/Library/WebServer/Documents/vod/fileSequence%d.ts" -hls_playlist_type vod /Library/WebServer/Documents/vod/prog_index.m3u8
+
+
+// -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease
+// -c:a aac
+// -ar 48000
+// -b:a 128k
+// -c:v h264 
+// -profile:v main 
+// -crf 20 -g 48 
+// -keyint_min 48 
+// -sc_threshold 0 
+// -b:v 6000k 
+// -maxrate 7800k 
+// -bufsize 3750k 
+// -hls_time 6 
+// -hls_playlist_type vod 
+// -hls_segment_filename hls-tos-teaser-1080p-%03d.ts 
+// hls-tos-teaser-prog-1080p.m3u8
+
+
 
 
 // ffmpeg(input_file)
 //   .outputOptions(
+//     '-vf', 'scale=w=1920:h=1080:force_original_aspect_ratio=decrease',
+//     '-c:a', 'aac',
+//     '-ar', '48000',
+//     '-b:a', '128k',
+//     '-c:v', 'h264', 
+//     '-profile:v', 'main', 
+//     '-crf', '20',
+//     '-g', '48',
+//     '-keyint_min', '48', 
+//     '-sc_threshold', '0', 
+//     '-b:v', '6000k', 
+//     '-maxrate', '7800k', 
+//     '-bufsize', '3750k', 
 //     '-hls_time', '6',
-//     '-hls_segment_filename', 'fileSequenced%d.ts',
+//     '-hls_segment_filename', 'hls-tos-teaser-1080p-%03d.ts',
 //     '-hls_playlist_type', 'vod',
 //   )
-//   .output("prog_index.m3u8")
+//   .output("hls-tos-teaser-prog-1080p.m3u8")
 //   .on('start', function (commandLine) {
 //     console.log('Spawned ffmpeg with command: ' + commandLine);
 //   })
@@ -55,21 +101,42 @@ ffmpeg.ffprobe(input_file, function (err, metadata) {
 //   })
 //   .run();
 
-ffmpeg(input_file)
-  .outputOptions(
-    '-vf', 'scale=w=200:h=320:force_original_aspect_ratio=decrease'
-  )
-  .output("temp.mp4")
-  .on('start', function (commandLine) {
-    console.log('Spawned ffmpeg with command: ' + commandLine);
-  })
-  .on('error', function (err) {
-    console.log('An error occurred: ' + err.message);
-  })
-  .on('end', function () {
-    console.log('Processing finished !');
-  })
-  .run();
+
+
+
+
+  // ffmpeg(input_file)
+  // .outputOptions(
+  //   '-hls_time', '6',
+  //   '-hls_segment_filename', 'fileSequenced%d.ts',
+  //   '-hls_playlist_type', 'vod',
+  // )
+  // .output("prog_index.m3u8")
+  // .on('start', function (commandLine) {
+  //   console.log('Spawned ffmpeg with command: ' + commandLine);
+  // })
+  // .on('error', function (err) {
+  //   console.log('An error occurred: ' + err.message);
+  // })
+  // .on('end', function () {
+  //   console.log('Processing finished !');
+  // })
+  // .run();
+// ffmpeg(input_file)
+//   .outputOptions(
+//     '-vf', 'scale=w=200:h=320:force_original_aspect_ratio=decrease'
+//   )
+//   .output("temp.mp4")
+//   .on('start', function (commandLine) {
+//     console.log('Spawned ffmpeg with command: ' + commandLine);
+//   })
+//   .on('error', function (err) {
+//     console.log('An error occurred: ' + err.message);
+//   })
+//   .on('end', function () {
+//     console.log('Processing finished !');
+//   })
+//   .run();
 
 
 // fs.readFile("input_mp4.txt", "utf8", function (error, data) {
